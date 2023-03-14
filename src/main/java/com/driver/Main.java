cb14b3acf63e8397e8c67de6b22538588baa3f5b
nkshrivas <nk733095@gmail.com>
2023-03-14 21:33:36 +0530

    New code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        RWOnly r = new RWOnly();
+//        String name = r.name;
+//        System.out.println(name);
+//       Error: java: name has private access in com.driver.RWOnly
+
+        r.setName("Nikhil's Assignment");
+        System.out.println(r.getName());
+
+
+    }
   
 }
